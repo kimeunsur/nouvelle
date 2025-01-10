f3372e75696a5b8df6450dac14833d5005cfc5a1
Doun Lee <jusmint3@gmail.com>
2025-01-11 03:06:10 +0900

    minor change

--- src/systems/Requests.tsx
+++ src/systems/Requests.tsx
@@ -1,6 +1,6 @@
 class RequestSys {
     getNaverUser = async () => {
-        const req = await fetch('http://localhost:5000/naver_auth/naver-login');
+        await fetch('http://localhost:5000/naver_auth/naver-login');
     }
 }
 
