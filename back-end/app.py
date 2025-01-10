70420f68860c49a9f3de911cfed5170883ed129b
Doun Lee <jusmint3@gmail.com>
2025-01-10 23:46:33 +0900

    minor changes

--- back-end/app.py
+++ back-end/app.py
@@ -10,6 +10,6 @@ app.config['MONGO_URI'] = "mongodb://localhost:27017"
 mongo = PyMongo(app)
 
 app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(naver_bp, url_prefix='/naver')
+app.register_blueprint(naver_bp, url_prefix='/naver_auth')
 if __name__ == '__main__':
     app.run(debug=True)
--- src/systems/Requests.tsx
+++ src/systems/Requests.tsx
@@ -1,6 +1,6 @@
 class RequestSys {
     getNaverUser = async () => {
-        const req = await fetch('mongodb://localhost:27017/naver-login');
+        const req = await fetch('http://localhost:5000/naver_auth/naver-login');
     }
 }
 
